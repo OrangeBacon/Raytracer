4ec33271cd4458b084466440454c3c3473c2dcda
OrangeBacon <computer.backup.15@gmail.com>
2022-08-03 15:26:14 +0100

    matrix4x4 struct

---
+++ src/geometry/matrix4x4.rs
@@ -0,0 +1,193 @@
+use std::ops::{Mul, MulAssign};
+
+use crate::geometry::Float;
+
+/// 4 by 4 floating point matrix
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Matrix4x4 {
+    pub data: [[Float; 4]; 4],
+}
+
+impl Matrix4x4 {
+    pub const IDENTITY: Self = Self {
+        data: [
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ],
+    };
+
+    /// Construct a matrix [row1, row2, ...]
+    pub fn from_array(data: [Float; 16]) -> Self {
+        Self {
+            data: [
+                [data[0], data[1], data[2], data[3]],
+                [data[4], data[5], data[6], data[7]],
+                [data[8], data[9], data[10], data[11]],
+                [data[12], data[13], data[14], data[15]],
+            ],
+        }
+    }
+
+    /// Construct a matrix [row1, row2, ...] from a slice of elements
+    /// panics if the slice does not contain at least 16 elements
+    pub fn from_slice(data: &[Float]) -> Self {
+        Self::from_array(data.try_into().unwrap())
+    }
+
+    /// Get the transpose of the matrix
+    #[rustfmt::skip]
+    pub fn transpose(&self) -> Self {
+        Self::from_array([
+            self.data[0][0], self.data[1][0], self.data[2][0], self.data[3][0],
+            self.data[0][1], self.data[1][1], self.data[2][1], self.data[3][1],
+            self.data[0][2], self.data[1][2], self.data[2][2], self.data[3][2],
+            self.data[0][3], self.data[1][3], self.data[2][3], self.data[3][3],
+        ])
+    }
+
+    /// Calculate the inverse of the matrix, mat ^ -1
+    /// returns None if the inverse does not exist
+    /// implementation ported from
+    /// https://github.com/mmp/pbrt-v3/blob/aaa552a4b9cbf9dccb71450f47b268e0ed6370e2/src/core/transform.cpp#L82
+    /// (numerically stable Gauss–Jordan elimination)
+    pub fn inverse(&self) -> Option<Matrix4x4> {
+        let mut indxc = [0usize; 4];
+        let mut indxr = [0usize; 4];
+        let mut ipiv = [0usize; 4];
+        let mut minv = self.data;
+
+        for i in 0..4 {
+            let mut irow = 0;
+            let mut icol = 0;
+            let mut big = 0.0;
+
+            // choose pivot
+            for j in 0..4 {
+                if ipiv[j] != 1 {
+                    for k in 0..4 {
+                        if ipiv[k] == 0 {
+                            if minv[j][k].abs() >= big {
+                                big = minv[j][k].abs();
+                                irow = j;
+                                icol = k;
+                            }
+                        } else if ipiv[k] > 1 {
+                            return None;
+                        }
+                    }
+                }
+            }
+
+            ipiv[icol] += 1;
+
+            // swap rows for pivot
+            if irow != icol {
+                for k in 0..4 {
+                    let tmp = minv[irow][k];
+                    minv[irow][k] = minv[icol][k];
+                    minv[icol][k] = tmp;
+                }
+            }
+            indxr[i] = irow;
+            indxc[i] = icol;
+
+            if minv[icol][icol] == 0.0 {
+                return None;
+            }
+
+            // set m[icol][icol] to one by scaling row icol
+            let pivinv = 1.0 / minv[icol][icol];
+            minv[icol][icol] = 1.0;
+            for j in 0..4 {
+                minv[icol][j] *= pivinv;
+            }
+
+            // subtract this row from others to zero out their columns
+            for j in 0..4 {
+                if j != icol {
+                    let save = minv[j][icol];
+                    minv[j][icol] = 0.0;
+
+                    for k in 0..4 {
+                        minv[j][k] -= minv[icol][k] * save;
+                    }
+                }
+            }
+        }
+
+        for j in (0..=3).rev() {
+            if indxr[j] != indxc[j] {
+                for k in 0..4 {
+                    minv[k].swap(indxr[j], indxc[j]);
+                }
+            }
+        }
+
+        Some(Matrix4x4 { data: minv })
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn mat_inv() {
+    let a = format!(
+        "{:.2?}",
+        Matrix4x4::from_array([
+            1.0, 4.0, 5.0, -1.0, -2.0, 3.0, -1.0, 0.0, 2.0, 1.0, 1.0, 0.0, 3.0, -1.0, 2.0, 1.0
+        ])
+        .inverse()
+        .unwrap()
+    );
+    let b = format!(
+        "{:.2?}",
+        Matrix4x4::from_array([
+            -0.1, -0.1, 0.6, -0.1, 0.0, 0.25, 0.25, 0.0, 0.2, -0.05, -0.45, 0.2, -0.1, 0.65, -0.65,
+            0.9
+        ])
+    );
+    assert_eq!(a, b);
+
+    let a = format!(
+        "{:.2?}",
+        Matrix4x4::from_array([
+            1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0
+        ])
+        .inverse()
+        .unwrap()
+    );
+    let b = format!(
+        "{:.2?}",
+        Matrix4x4::from_array([
+            0.25, 0.25, 0.25, -0.25, 0.25, 0.25, -0.25, 0.25, 0.25, -0.25, 0.25, 0.25, -0.25, 0.25,
+            0.25, 0.25,
+        ])
+    );
+    assert_eq!(a, b)
+}
+
+impl Mul for Matrix4x4 {
+    type Output = Matrix4x4;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut data: [[Float; 4]; 4] = Default::default();
+
+        for i in 0..4 {
+            for j in 0..4 {
+                data[i][j] = self.data[i][0] * rhs.data[0][j]
+                    + self.data[i][1] * rhs.data[1][j]
+                    + self.data[i][2] * rhs.data[2][j]
+                    + self.data[i][3] * rhs.data[3][j]
+            }
+        }
+
+        Matrix4x4 { data }
+    }
+}
+
+impl MulAssign for Matrix4x4 {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,4 +1,6 @@
+mod bounds2;
 mod bounds3;
+mod matrix4x4;
 mod normal3;
 mod number;
 mod point2;
@@ -6,7 +8,6 @@ mod point3;
 mod ray;
 mod vector2;
 mod vector3;
-mod bounds2;
 
 #[cfg(feature = "double")]
 pub type Float = f64;
@@ -41,7 +42,56 @@ pub use bounds3::Bounds3;
 pub use bounds3::Bounds3f;
 pub use bounds3::Bounds3i;
 
-
 pub use bounds2::Bounds2;
 pub use bounds2::Bounds2f;
 pub use bounds2::Bounds2i;
+
+pub use matrix4x4::Matrix4x4;
+
+/// Linearly interpolate between two floats
+pub fn lerp(t: Float, a: Float, b: Float) -> Float {
+    (1.0 - t) * a + t * b
+}
+
+/// Solve a quadratic equation ax^2 + bx + c = 0
+/// If no real solutions are found, returns None.  If both solutions are the
+/// same, that solution will be both return values.
+pub fn quadratic(a: Float, b: Float, c: Float) -> Option<(Float, Float)> {
+    let a = a as f64;
+    let b = b as f64;
+    let c = c as f64;
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        return None;
+    }
+    let root = discriminant.sqrt();
+
+    let q = if b < 0.0 {
+        -0.5 * (b - root)
+    } else {
+        -0.5 * (b + root)
+    };
+
+    let t0 = q / a;
+    let t1 = c / q;
+
+    Some((t0.min(t1) as _, t0.max(t1) as _))
+}
+
+/// Solve equation Ax = B where A is a 2x2 matrix and B is a 2 element vector
+/// returns x as (x1, x2) if solution found, otherwise None
+pub fn solve_2x2_system(a: [[Float; 2]; 2], b: [Float; 2]) -> Option<(Float, Float)> {
+    let det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+    if det.abs() < 1e-10 {
+        return None;
+    }
+
+    let x0 = (a[1][1] * b[0] - a[0][1] * b[1]) / det;
+    let x1 = (a[0][0] * b[1] - a[1][0] * b[0]) / det;
+
+    if x0.is_nan() || x1.is_nan() {
+        return None;
+    }
+
+    Some((x0, x1))
+}
