18cb38b090554e86df7715890abe1fa5210ea80e
OrangeBacon <computer.backup.15@gmail.com>
2022-08-15 13:45:41 +0100

    Global api stuff

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use api::{Options, Pbrt};
+
+fn main() -> Result<(), ()> {
+    let options = Options {
+        thread_count: 1,
+        quick_render: true,
+        quiet: false,
+        verbose: true,
+        image_file: "image.png".into(),
+    };
+
+    let mut renderer = Pbrt::new(options);
+
+    for arg in std::env::args() {
+        renderer.parse_pbrt(arg)?;
+    }
+
+    renderer.render();
+
+    Ok(())
 }
