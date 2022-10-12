65f1772d25efab2a4e004d832b8a61f31a0ef407
OrangeBacon <computer.backup.15@gmail.com>
2022-10-12 20:19:49 +0100

    Remove old raytracer

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,25 +1,9 @@
-mod aabb;
-mod bvh;
-mod camera;
-mod material;
-mod perlin;
-mod ray;
-mod ray_trace;
-mod scene;
-mod scene_format;
-mod shapes;
-mod texture;
-mod utils;
-
-use std::{path::PathBuf, sync::Arc};
+use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::Parser;
-use glam::DVec3;
-use parsers::parse_pbrt;
-use rand::Rng;
 
-use crate::{camera::Camera, ray::Ray, scene::Scene, shapes::Hittable};
+use parsers::parse_pbrt;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -41,49 +25,5 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    let scene = std::fs::read_to_string(&args.scene)?;
-    let scene = Scene::from_file(toml::from_str(&scene)?)?;
-
-    let camera = Camera::new(&scene);
-
-    ray_trace::ray_trace(scene.width, scene.height, scene.gamma, |x, y| {
-        let mut colour = DVec3::ZERO;
-        let mut rng = rand::thread_rng();
-        for _ in 0..scene.samples_per_pixel {
-            let u = (x as f64 + rng.gen::<f64>()) / scene.width as f64;
-            let v = (y as f64 + rng.gen::<f64>()) / scene.height as f64;
-
-            let ray = camera.ray(u, v);
-            colour += ray_colour(
-                ray,
-                scene.background_colour,
-                &scene.world,
-                scene.recursive_depth,
-            );
-        }
-
-        colour / scene.samples_per_pixel as f64
-    })
-    .save(&args.output)?;
-
     Ok(())
-}
-
-fn ray_colour(ray: Ray, background: DVec3, world: &Arc<dyn Hittable>, depth: u32) -> DVec3 {
-    if depth == 0 {
-        return DVec3::ZERO;
-    }
-
-    if let Some(record) = world.hit(ray, 0.001, f64::INFINITY) {
-        let emitted = record.material.emitted(record.uv, record.point);
-
-        if let Some(scatter) = record.material.scatter(ray, &record) {
-            return emitted
-                + scatter.attenuation
-                    * ray_colour(scatter.scattered, background, world, depth - 1);
-        }
-        return emitted;
-    }
-
-    background
 }
